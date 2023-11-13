febb616b2f94b75b8e609ddebe446d93cc0412c9
Joaov511 <jv232479@gmail.com>
2023-11-13 14:32:34 -0300

    arrow to go back to top and other responsiviness problems fixed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var portiflioSiteBtn = document.getElementsByClassName('portifolioSiteBtn');
 var portifolioCard = document.getElementsByClassName('portifolioCard');
-var sideProjectsUrl = ['none', 'https://joaov511.github.io/Sorting-Algorithms-Site/'];
+var arrowToTop = document.getElementById('arrowToTop');
+var portifolioTitle = document.getElementById('potifolioTitle');
+var sideProjectsUrl = ['', 'https://joaov511.github.io/Sorting-Algorithms-Site/'];
 var _loop_1 = function (i) {
     portiflioSiteBtn[i].addEventListener('click', function () {
         window.open(sideProjectsUrl[i]);
@@ -9,3 +11,11 @@ var _loop_1 = function (i) {
 for (var i = 0; i < portiflioSiteBtn.length; i++) {
     _loop_1(i);
 }
+document.addEventListener('scroll', function () {
+    if (window.scrollY > 700) {
+        arrowToTop.style.display = 'flex';
+    }
+    else {
+        arrowToTop.style.display = 'none';
+    }
+});
--- script.ts
+++ script.ts
@@ -1,10 +1,21 @@
 const portiflioSiteBtn = document.getElementsByClassName('portifolioSiteBtn');
 const portifolioCard = document.getElementsByClassName('portifolioCard');
+const arrowToTop = document.getElementById('arrowToTop');
+const portifolioTitle = document.getElementById('potifolioTitle');
 
-let sideProjectsUrl: Array<string> = ['none','https://joaov511.github.io/Sorting-Algorithms-Site/'];
+let sideProjectsUrl: Array<string> = ['','https://joaov511.github.io/Sorting-Algorithms-Site/'];
 
 for(let i : number = 0; i < portiflioSiteBtn.length; i++) {
     portiflioSiteBtn[i].addEventListener('click', function() {
         window.open(sideProjectsUrl[i]);
     })
 }
+
+document.addEventListener('scroll', function() {
+    if(window.scrollY > 700) {
+        arrowToTop.style.display = 'flex';
+    }
+    else {
+        arrowToTop.style.display = 'none';
+    }
+})
